72a6d07387839554c25a1e47265295b18d2d7f40
KhalidPC <khalid.ahmad063@gmail.com>
2024-01-07 10:33:06 +0600

    pdf print project has been going on 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF Print Project Two',
+      title: 'PDF Print Project',
       theme: ThemeData(
         // This is the theme of your application.
         //
